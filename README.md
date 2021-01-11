9ce204288e5e2dc4dbabeb0a5ed5ed009ece2e55
heyimrendy <61161733+heyimrendy@users.noreply.github.com>
2021-01-11 00:15:12 +0000

    Init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # twitch-check-live
-Check followed channels live on Twitch
+Check followed channels live on Twitch from terminal.
+
+**WARNING: User OAuth token and unofficial API usage are discouraged. Use at your own risk!**
+
+# Requirements
+This script require Powershell 7 for ternary operator. If you wish to use older Powershell, edit line 34.
+
+# Preview
+![](https://i.nuuls.com/DVHPJ.png)
+
+
+# Get Started
+Setting up is extremely easy. Edit line 1 of `twitch-live.ps1` and add user OAuth token.
+
+### Tips
+1. By default max channels set to 50. Edit line 2 if you prefer custom. Value between 1-50 to mimic the website.
+2. Custom User-Agent in line 3. Check https://www.whatismybrowser.com/guides/the-latest-user-agent/ for generic User-Agent.
+3. Don't execute the script too frequent, might raise detection by Twitch.
---
+++ twitch-live.ps1
@@ -0,0 +1,45 @@
+$authToken = ""
+$total = 50
+$userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"
+
+$uri = "https://gql.twitch.tv/gql"
+$headers = @{
+    "Accept" = "*/*"
+    "Accept-Encoding" = "gzip, deflate, br"
+    "Accept-Language" = "en-US"
+    "Authorization" = $authToken
+    "Client-Id" = "kimne78kx3ncx6brgo4mv6wki5h1ko"
+    "Connection" = "keep-alive"
+    "Content-Type" = "text/plain;charset=UTF-8"
+    "DNT" = 1
+    "Host" = "gql.twitch.tv"
+    "Origin" = "https://www.twitch.tv"
+    "User-Agent" = $userAgent
+}
+
+$gqlBody = "{`"query`":`"query{currentUser{followedLiveUsers(first:$($total)){edges{node{login stream{viewersCount game{displayName} title createdAt}}}}}}`"}"
+
+try {
+    $gqlResponse = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body $gqlBody
+
+    if(!$gqlResponse.errors){
+        if ($gqlResponse.data.currentUser.followedLiveUsers.edges.Count -lt 1) {
+            Write-Host "All offline!"
+        } else {
+            foreach ($nodeValue in $gqlResponse.data.currentUser.followedLiveUsers.edges) {
+                Write-Host "[*] $($nodeValue.node.login)" -NoNewline
+                Write-Host " ($($nodeValue.node.stream.game.displayName))" -NoNewline -ForeGroundColor Gray
+                Write-Host " | " -NoNewline
+                Write-Host "$("{0:N0}" -f $nodeValue.node.stream.viewersCount)" -NoNewline -ForegroundColor Red
+                Write-Host " $(($nodeValue.node.stream.viewersCount -lt 2) ? 'viewer' : 'viewers') " -NoNewline
+                Write-Host "| $((New-TimeSpan -End "$((Get-Date).ToUniversalTime())" -Start $nodeValue.node.stream.createdAt).ToString("hh\:mm\:ss"))"
+            }
+        }
+    } else {
+        Write-Host "Error! $($gqlResponse.errors.message)"
+    }
+} catch {
+    Write-Host "Error: $($_.Exception.Response.ReasonPhrase)"
+    Write-Host "StatusCode: $($_.Exception.Response.StatusCode.value__)"
+    # Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+}
